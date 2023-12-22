7908054c1ff5f3103208836d2280249ff7a95d93
febrianth <pekmbiee14@gmail.com>
2023-12-22 16:21:10 +0700

    tambah halaman disabilitas, lansia

---
+++ public/partials/wp-siks-manajemen-disabilitas.php
@@ -0,0 +1,51 @@
+<?php echo "test";
+?>
+<h1 class="text-center">Manajemen Data Disabillitas</h1>
+
+<script>
+    jQuery(document).ready(function() {
+        getDataTables();
+    });
+
+    function getDataTables() {
+        if (typeof dataTableInstance === 'undefined') {
+            window.dataTableInstance = jQuery('#new_table_element').DataTable({
+                "processing": true,
+                "serverSide": true,
+                "ajax": {
+                    url: 'URL_ENDPOINT', 
+                    type: 'post',
+                    dataType: 'json',
+                    data: {
+                        'action': 'get_data_from_server', 
+                        'api_key': 'YOUR_API_KEY', 
+                        'year': 'YEAR_VALUE', 
+                        'department_id': 'DEPARTMENT_ID' 
+                    }
+                },
+                lengthMenu: [
+                    [20, 50, 100, -1],
+                    [20, 50, 100, "All"]
+                ],
+                order: [
+                    [0, 'asc']
+                ],
+                "drawCallback": function(settings) {
+                    jQuery("#loading_element").hide(); 
+                },
+                "columns": [{
+                        "data": 'year',
+                        className: "text-center"
+                    },
+                    {
+                        "data": 'district',
+                        className: "text-center"
+                    },
+                    
+                ]
+            });
+        } else {
+            dataTableInstance.draw();
+        }
+    }
+</script>
