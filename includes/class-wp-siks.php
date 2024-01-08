38eae004aa8ef33bc7f19d2935799a2c280c80c4
dzawata <interisty91@gmail.com>
2024-01-09 05:19:48 +0700

    tambah filter nik atau kk kosong, kecamatan, desa, kolom update terakhir

--- includes/class-wp-siks.php
+++ includes/class-wp-siks.php
@@ -173,6 +173,8 @@ class Wp_Siks {
   		$this->loader->add_action( 'wp_ajax_import_excel_odgj',  $plugin_admin, 'import_excel_odgj');
   		$this->loader->add_action( 'wp_ajax_get_data_dtks_siks',  $plugin_admin, 'get_data_dtks_siks');
   		$this->loader->add_action( 'wp_ajax_export_excel_data_dtks_siks',  $plugin_admin, 'export_excel_data_dtks_siks');
+  		$this->loader->add_action( 'wp_ajax_get_data_kecamatan_siks',  $plugin_admin, 'get_data_kecamatan_siks');
+  		$this->loader->add_action( 'wp_ajax_get_data_desa_siks',  $plugin_admin, 'get_data_desa_siks');
 
 		$this->loader->add_action('carbon_fields_register_fields', $plugin_admin, 'crb_attach_siks_options');
 		
